ef6caf6928205f360de78b0839456a94565d8c28
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-01-29 22:37:29 +0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/steps-tracker/", // Укажите название вашего репозитория
+  base: "/REACT_steps-tracker/", // Укажите название вашего репозитория
 });
